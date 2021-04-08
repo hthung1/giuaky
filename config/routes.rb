138203caef077ced0ec780d34613da127b43a90d
hthung1 <54886568+hthung1@users.noreply.github.com>
2021-04-08 19:17:46 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :loai_thuocs
   resources :nccs
   resources :sxes
   resources :thuocs
+
+  get '/',to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
